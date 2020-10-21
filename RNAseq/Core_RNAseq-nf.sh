97deb980ec8ccd7006a07e152e26d4b5e69700af
mzamanian <mzamanian@SVM-PBS-3617.local>
2020-10-20 22:54:45 -0500

    multiline command

--- RNAseq/Core_RNAseq-nf.sh
+++ RNAseq/Core_RNAseq-nf.sh
@@ -17,7 +17,8 @@ git clone https://github.com/zamanianlab/Core_RNAseq-nf.git
 
 # run nextflow
 export NXF_OPTS='-Xms1g -Xmx8g'
-nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1 --star --release "WBPS14" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
+nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config \
+  --dir $1 --star --release "WBPS14" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
 
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work
