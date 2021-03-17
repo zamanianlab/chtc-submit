c9dffb832962e29f7f4980f0854b080af34069c6
Mostafa Zamanian <zamanian.wisc@gmail.com>
2021-03-17 18:52:35 -0500

    di > bm

--- RNAseq/Core_RNAseq-nf.sh
+++ RNAseq/Core_RNAseq-nf.sh
@@ -19,10 +19,9 @@ git clone https://github.com/zamanianlab/Core_RNAseq-nf.git
 
 # run nextflow (QC, star)
 export NXF_OPTS='-Xms1g -Xmx8g'
-#nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1\
-#  --star --release "WBPS15" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
 nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1\
-   --star --release "WBPS15" --species "dirofilaria_immitis" --prjn "PRJEB1797" --rlen "150"
+  --star --release "WBPS15" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
+#   --star --release "WBPS15" --species "dirofilaria_immitis" --prjn "PRJEB1797" --rlen "150"
 
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work input
