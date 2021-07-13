4c355d5625cebab32f6683e98f6ea28432c2bc82
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2021-07-13 13:12:08 -0500

    hello world worked, revert

--- cwoh/covid_seq.sh
+++ cwoh/covid_seq.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-echo 'hello world'
+# set home () and mk dirs
+export HOME=$PWD
+mkdir input work output
 
-# # set home () and mk dirs
-# export HOME=$PWD
-# mkdir input work output
-#
-# # echo core, thread, and memory
-# echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
-# grep 'cpu cores' /proc/cpuinfo | uniq
-# echo $(free -g)
-#
-# # transfer and decompress input data from staging ($1 is ${dir} from args)
-# cp -r /staging/groups/zamanian_group/input/$1.tar input
-# cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd $HOME
-#
-# # clone nextflow git repo
-# git clone --branch nw https://github.com/alceballosa/cw_onehealth_protocols.git
-#
-# # run nextflow (QC, star)
-# # export NXF_OPTS='-Xms1g -Xmx8g'
-# nextflow run covid_cw_onehealth_protocols/dsl2.nf \
-#   -c cw_onehealth_protocols/chtc-covid.conf -w work \
-#   --source_folder input/$1 --output_folder output/$1 \
-#   --primers_folder cw_onehealth_protocols/primer_schemes/nCoV-2019/V3
-#
-# # rm files you don't want transferred back to /home/{net-id}
-# rm -r work input
-#
-# # tar output folder and delete it
-# cd output && tar -cvf $1.tar $1 && rm -r $1 && cd $HOME
-#
-# # remove staging output tar if there from previous run
-# rm -f /staging/groups/zamanian_group/output/$1.tar
-#
-# # mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
-# mv output/$1.tar /staging/groups/zamanian_group/output/
+# echo core, thread, and memory
+echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
+grep 'cpu cores' /proc/cpuinfo | uniq
+echo $(free -g)
+
+# transfer and decompress input data from staging ($1 is ${dir} from args)
+cp -r /staging/groups/zamanian_group/input/$1.tar input
+cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd $HOME
+
+# clone nextflow git repo
+git clone --branch nw https://github.com/alceballosa/cw_onehealth_protocols.git
+
+# run nextflow (QC, star)
+# export NXF_OPTS='-Xms1g -Xmx8g'
+nextflow run covid_cw_onehealth_protocols/dsl2.nf \
+  -c cw_onehealth_protocols/chtc-covid.conf -w work \
+  --source_folder input/$1 --output_folder output/$1 \
+  --primers_folder cw_onehealth_protocols/primer_schemes/nCoV-2019/V3
+
+# rm files you don't want transferred back to /home/{net-id}
+rm -r work input
+
+# tar output folder and delete it
+cd output && tar -cvf $1.tar $1 && rm -r $1 && cd $HOME
+
+# remove staging output tar if there from previous run
+rm -f /staging/groups/zamanian_group/output/$1.tar
+
+# mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
+mv output/$1.tar /staging/groups/zamanian_group/output/
