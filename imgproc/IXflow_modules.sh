026b72801c4fac10c9eec9f956d7ffe938e4e35a
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2021-09-09 09:44:10 -0500

    fix wormsize pipeline

--- imgproc/IXflow_modules.sh
+++ imgproc/IXflow_modules.sh
@@ -2,16 +2,9 @@
 
 # set home () and mk dirs
 export HOME=$PWD
-mkdir metadata
-# mkdir output/$1
-# mkdir work/$1
-
-git clone https://github.com/zamanianlab/Core_imgproc.git
-mkdir Core_imgproc/CP/projects
-mkdir Core_imgproc/CP/projects/$1
-mkdir Core_imgproc/CP/projects/$1/raw_images
-input="Core_imgproc/CP/projects/$1/raw_images"
-echo $input
+mkdir input metadata work output
+mkdir output/$1
+mkdir work/$1
 
 # echo core, thread, and memory
 echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
@@ -19,12 +12,16 @@ grep 'cpu cores' /proc/cpuinfo | uniq
 echo $(free -g)
 
 # transfer and decompress input data from staging ($1 is ${dir} from args)
-cp -r /staging/groups/zamanian_group/input/$1.tar $input
-cd $input && tar --strip-components 5 -xvf $1.tar && cd $HOME
+cp -r /staging/groups/zamanian_group/input/$1.tar input
+cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd ..
 
 # transfer and decompress metadata from staging ($1 is ${dir} from args)
 cp -r /staging/groups/zamanian_group/metadata/$1.tar metadata
-cd metadata && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd $HOME
+cd metadata && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd ..
+
+# for testing
+# scp -r njwheeler@transfer.chtc.wisc.edu:/staging/groups/zamanian_group/subsampled/20201118-p01-MZ_172_sub.tar subsampled/
+# cd subsampled && tar -xvf 20201118-p01-MZ_172_sub.tar && rm 20201118-p01-MZ_172_sub.tar && cd
 
 # get basename and plate IX metadata (from HTD file)
 base=`echo $1 | cut -d"_" -f1`
@@ -51,21 +48,29 @@ echo "y_sites: ${y_sites}"
 echo "NWavelengths: ${NWavelengths}"
 echo "WaveNames: ${WaveNames}"
 
-# generate the CSV of files + metadata
-Rscript Core_imgproc/CP/scripts/generate_filelist_wormsize.R $1
+# clone nextflow git repo
+git clone https://github.com/zamanianlab/Core_imgproc.git
 
 # run script
-cellprofiler -c -r -p Core_imgproc/CP/pipelines/wormsize.cppipe --data-file=Core_imgproc/CP/metadata/image_paths_wormsize.csv
+python Core_imgproc/IXflow/chtc-ix_optical_flow_mf.py \
+  input/$1 \
+  output/$1 \
+  work/$1 \
+  $rows \
+  $columns \
+  $time_points #\
+  #--reorganize
 
-# join metatdata, tar output folder, and delete it
-mv metadata output
-Rscript Core_imgproc/CP/scripts/metadata_join_wormsize.R $1
-rm -r output/metadata
-mv output $1
-tar -cvf $1.tar $1 && rm -r $1
+Rscript Core_imgproc/metadata_join.R $1
+
+# rm files you don't want transferred back to /home/{net-id}
+rm -r work input metadata
+
+# tar output folder and delete it
+cd output && tar -cvf $1.tar $1 && rm -r $1 && cd ..
 
 # remove staging output tar if there from previous run
 rm -f /staging/groups/zamanian_group/output/$1.tar
 
 # mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
-mv $1.tar /staging/groups/zamanian_group/output/
+mv output/$1.tar /staging/groups/zamanian_group/output/
