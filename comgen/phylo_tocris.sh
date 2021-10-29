d90a3c96fbb0c7264d98d497c83f92a2a7f6d45b
Katie Ryan <ktryan2@wisc.edu>
2021-10-29 12:57:51 -0500

    create tocris phylo scripts

---
+++ comgen/phylo_tocris.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# set home () and mk dirs
+export HOME=$PWD
+mkdir input work output
+
+# echo core, thread, and memory
+echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
+grep 'cpu cores' /proc/cpuinfo | uniq
+echo $(free -g)
+
+# transfer and decompress input data from staging ($1 is ${dir} from args)
+# cp -r /staging/groups/zamanian_group/input/$1.tar $HOME
+# cd $HOME && tar -xvf $1.tar && rm $1.tar
+
+# clone nextflow git repo
+git clone https://github.com/zamanianlab/Phylogenetics.git
+
+# cd $HOME
+# echo $(ls -lh)
+
+# run script
+bash Phylogenetics/Tocris/pipeline.sh
+
+# rm files you don't want transferred back to /home/{net-id}
+rm -r work input
+
+# tar output folder and delete it
+tar -cvf tocris.tar output && rm -r output
+
+# remove staging output tar if there from previous run
+rm -f /staging/groups/zamanian_group/output/tocris.tar
+
+# mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
+mv tocris.tar /staging/groups/zamanian_group/output/
