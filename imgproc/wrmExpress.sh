c06c0a0660ecba3633397b9e5294b4c3018f89ba
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2021-11-03 15:19:45 -0500

    add a 2nd argument to the python command

--- imgproc/wrmExpress.sh
+++ imgproc/wrmExpress.sh
@@ -2,13 +2,12 @@
 
 # set home () and mk dirs
 export HOME=$PWD
+mkdir input
 mkdir metadata
 mkdir output/
 mkdir work/
 
 git clone https://github.com/zamanianlab/Core_imgproc.git
-input="input/$1"
-echo $input
 
 # echo core, thread, and memory
 echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
@@ -16,15 +15,20 @@ grep 'cpu cores' /proc/cpuinfo | uniq
 echo $(free -g)
 
 # transfer and decompress input data from staging ($1 is ${dir} from args)
-cp -r /staging/groups/zamanian_group/input/$1.tar $input
-cd $input && tar --strip-components 5 -xvf $1.tar && cd $HOME
+tar -xf /staging/groups/zamanian_group/input/$1.tar -C input/
+
+# deprecated
+# cp -r /staging/groups/zamanian_group/input/$1.tar input
+# cd $input && tar --strip-components 5 -xvf $1.tar && cd $HOME
 
 # transfer and decompress metadata from staging ($1 is ${dir} from args)
-cp -r /staging/groups/zamanian_group/metadata/$1.tar metadata
-cd metadata && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd $HOME
+tar -xf /staging/groups/zamanian_group/metadata/$1.tar -C metadata
+
+# deprecated
+# cd metadata && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd $HOME
 
 # run the wrapper
-python Core_imgproc/wrapper.py Core_imgproc/parameters.yml
+python Core_imgproc/wrapper.py Core_imgproc/parameters.yml $1
 
 # tar output folder and delete it
 mv output $1
