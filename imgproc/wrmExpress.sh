21741f5794cb12e223647c4684e81e5507a73521
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2021-11-05 15:14:41 -0500

    fix git clone command

--- imgproc/wrmExpress.sh
+++ imgproc/wrmExpress.sh
@@ -7,7 +7,7 @@ mkdir metadata
 mkdir output/
 mkdir work/
 
-git --branch packagify clone https://github.com/zamanianlab/Core_imgproc.git
+git clone  --branch packagify https://github.com/zamanianlab/Core_imgproc.git
 
 # echo core, thread, and memory
 echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
