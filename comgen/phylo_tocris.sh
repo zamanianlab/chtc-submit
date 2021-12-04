dc1bc5106bef4a741836bad64efbe7ec4d9910d0
Katie Ryan <ktryan2@wisc.edu>
2021-12-04 13:20:44 -0600

    add alignment and last loop from pipeline

--- comgen/phylo_tocris.sh
+++ comgen/phylo_tocris.sh
@@ -54,8 +54,8 @@ makeblastdb -in $proteomes/HsUniProt_nr.fasta -dbtype prot
 mv Phylogenetics/Tocris/Hs_seeds.list.txt work
 mkdir work/1_Hs_seeds
 seeds=work/1_Hs_seeds
-mkdir output/2_Hs_targets
-Hs_targets=output/2_Hs_targets
+mkdir work/2_Hs_targets
+Hs_targets=work/2_Hs_targets
 mkdir output/alignments
 alignments=output/alignments
 mv Phylogenetics/Tocris/parasite_db.list.txt work
@@ -78,6 +78,29 @@ blastp -query $seeds/Hs_seeds."$line_sub".fasta -db $proteomes/HsUniProt_nr.fast
 cat $Hs_targets/"$line_sub".out | awk '$3>30.000 && $11<1E-3 {print $2}' | sort | uniq > $Hs_targets/"$line_sub".list.txt
 seqtk subseq $proteomes/HsUniProt_nr.fasta $Hs_targets/"$line_sub".list.txt > $Hs_targets/"$line_sub".ext.fasta
 rm $Hs_targets/*.out
+
+cat $Hs_targets/"$line_sub".ext.fasta | sed 's/>/>Homo_sapiens|/g' > $alignments/"$line_sub".combined.fasta
+while IFS= read -r paradb; do
+    #blast expanded human targets against parasite dbs
+    para_name=$(echo "$paradb" | awk 'BEGIN { FS = "." } ; { print $1 }')
+    blastp -query $Hs_targets/"$line_sub".ext.fasta -db $proteomes/$paradb -out $Para_targets/"$line_sub"."$para_name".out -outfmt 6 -max_hsps 1 -evalue 1E-1 -num_threads 4
+		cat $Para_targets/"$line_sub"."$para_name".out | awk '$3>30.000 && $11<1E-3 {print $2}' | sort | uniq > $Para_targets/"$line_sub"."$para_name".list.txt
+		seqtk subseq $proteomes/$paradb $Para_targets/"$line_sub"."$para_name".list.txt > $Para_targets/"$line_sub"."$para_name".fasta
+    #blast parasite hits against human db
+    blastp -query $Para_targets/"$line_sub"."$para_name".fasta -db $proteomes/HsUniProt_nr.fasta -out $Para_recip/"$line_sub"."$para_name".out -outfmt 6 -max_hsps 1 -evalue 1E-3 -num_threads 4
+    cat $Para_recip/"$line_sub"."$para_name".out | awk '$3>30.000 && $11<1E-3 {print $1, $2}' | sort | uniq  > $Para_recip/"$line_sub"."$para_name".list.txt
+    #compare to original human list to find surviving parasite targets
+    grep -Ff $Hs_targets/"$line_sub".list.txt $Para_recip/"$line_sub"."$para_name".list.txt | awk '{print $1}' | sort | uniq > $Para_final/"$line_sub"."$para_name".list.txt
+    seqtk subseq $proteomes/$paradb $Para_final/"$line_sub"."$para_name".list.txt > $Para_final/"$line_sub"."$para_name".fasta
+    cat $Para_final/"$line_sub"."$para_name".fasta | sed 's/>/>'$para_name'|/g' >> $alignments/"$line_sub".combined.fasta
+    #align mafft
+    einsi --reorder --thread 2 $alignments/"$line_sub".combined.fasta > $alignments/"$line_sub".combined.aln
+    #trim
+    trimal -gt 0.7 -in $alignments/"$line_sub".combined.aln -out $alignments/"$line_sub".combined_trim.aln
+    trimal -resoverlap 0.70 -seqoverlap 70 -in $alignments/"$line_sub".combined_trim.aln -out $alignments/"$line_sub".combined_final.aln
+    #tree-building
+    iqtree-2.1.3-MacOSX/bin/iqtree2 -s $alignments/"$line_sub".combined_final.aln -nt 4 -alrt 1000 -bb 1000
+done < work/parasite_db.list.txt
 
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work input
