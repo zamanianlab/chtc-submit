14092c290498b2c845a754645611e6ddcada325c
Katie Ryan <ktryan2@wisc.edu>
2022-02-09 15:35:20 -0600

    add $line_sub argument

--- comgen/phylo_tocris.sh
+++ comgen/phylo_tocris.sh
@@ -12,7 +12,7 @@ echo $(free -g)
 # split target ID
 printf '%s\n' $1
 echo $1 > work/Hs_seeds.txt
-Hs_target=$(echo $1 | awk 'BEGIN { FS = "|" } ; { print $3 }')
+line_sub=$(echo $1 | awk 'BEGIN { FS = "|" } ; { print $3 }')
 
 # clone nextflow git repo
 git clone https://github.com/zamanianlab/Phylogenetics.git
@@ -21,7 +21,7 @@ git clone https://github.com/zamanianlab/Phylogenetics.git
 # echo $(ls -lh)
 
 # run script
-bash Phylogenetics/Tocris/pipeline.sh
+bash Phylogenetics/Tocris/pipeline.sh $line_sub
 
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work input
