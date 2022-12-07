470ecf581c2d34effd228385c0f70c3dec5e58be
Mostafa Zamanian <zamanian.wisc@gmail.com>
2022-12-07 01:06:37 -0600

    allow for two directories

--- dnavc/Aeaeg-vc-nf.sh
+++ dnavc/Aeaeg-vc-nf.sh
@@ -9,9 +9,15 @@ echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
 grep 'cpu cores' /proc/cpuinfo | uniq
 echo $(free -g)
 
-# transfer and decompress input data from staging ($1 is ${dir} from args)
+# transfer and decompress input data from staging ($1 is ${dir1}, $2 is ${dir2} from args)
 cp -r /staging/groups/zamanian_group/input/$1.tar input
-cd input && tar -xvf $1.tar && rm $1.tar && cd .. #for globus RD structure
+cd input && tar -xvf $1.tar && rm $1.tar && cd ..
+cp -r /staging/groups/zamanian_group/input/$2.tar input
+cd input && tar -xvf $2.tar && rm $2.tar && cd ..
+
+# transfer fastq files to a common input folder (fqs)
+cd input && mkdir fqs
+mv $1/*.gz fqs && mv $2/*.gz fqs
 
 # transfer and decompress mosquito genome index data from staging 
 cp -r /staging/groups/zamanian_group/input/Aeaegypti_ref.tar input
@@ -22,16 +28,16 @@ git clone https://github.com/zamanianlab/DNAseq-VC-nf.git
 
 # run nextflow (QC, star)
 export NXF_OPTS='-Xms1g -Xmx8g'
-nextflow run DNAseq-VC-nf/Aeaeg-vc.nf -w work -c DNAseq-VC-nf/chtc.config --dir $1 --rlen "150" --qc
+nextflow run DNAseq-VC-nf/Aeaeg-vc.nf -w work -c DNAseq-VC-nf/chtc.config --dir $1_$2 --rlen "150" --qc
 
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work input
 
 # tar output folder and delete it
-cd output && tar -cvf $1.tar $1 && rm -r $1 && cd ..
+cd output && tar -cvf $1_$2.tar $1_$2 && rm -r $1_$2 && cd ..
 
 # remove staging output tar if there from previous run
-rm -f /staging/groups/zamanian_group/output/$1.tar
+rm -f /staging/groups/zamanian_group/output/$1_$2.tar
 
 # mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
-mv output/$1.tar /staging/groups/zamanian_group/output/
+mv output/$1_$2.tar /staging/groups/zamanian_group/output/
