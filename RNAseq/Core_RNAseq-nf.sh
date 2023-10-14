3fbaabe696ad1ad39a29c674ff8638b68bafbfd4
Clair Henthorn <clair.henthorn@gmail.com>
2023-10-14 08:58:24 -0500

    Uncomment nextflow for diro

--- RNAseq/Core_RNAseq-nf.sh
+++ RNAseq/Core_RNAseq-nf.sh
@@ -11,8 +11,8 @@ echo $(free -g)
 
 # transfer and decompress input data from staging ($1 is ${dir} from args)
 cp -r /staging/groups/zamanian_group/input/$1.tar input
-cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd .. #for RD structure
-#cd input && tar -xvf $1.tar && rm $1.tar && cd .. #for brc transfer no file structure
+#cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd .. #for RD structure
+cd input && tar -xvf $1.tar && rm $1.tar && cd .. #for brc transfer no file structure
 
 # clone nextflow git repo
 git clone https://github.com/zamanianlab/Core_RNAseq-nf.git
@@ -20,8 +20,8 @@ git clone https://github.com/zamanianlab/Core_RNAseq-nf.git
 # run nextflow (QC, star)
 export NXF_OPTS='-Xms1g -Xmx8g'
 nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1\
-  --star --release "WBPS15" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
-#   --star --release "WBPS15" --species "dirofilaria_immitis" --prjn "PRJEB1797" --rlen "150"
+#  --star --release "WBPS15" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
+   --star --release "WBPS18" --species "dirofilaria_immitis" --prjn "PRJEB1797" --rlen "150"
 
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work input
