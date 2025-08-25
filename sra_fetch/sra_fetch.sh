87e1729f0591ea33495c60820a4e50f9a836a127
Elena Rehborg <106282062+ejgrehborg@users.noreply.github.com>
2025-08-25 12:47:47 -0500

    Update sra_fetch.sh

--- sra_fetch/sra_fetch.sh
+++ sra_fetch/sra_fetch.sh
@@ -34,6 +34,6 @@ rm -r sra_files_output
 rm -f /staging/groups/zamanian_group/input/sra_reads.tar
 
 # mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
-mv sra_reads.tar /staging/groups/zamanian_group/input/
+mv sra_reads.tar /staging/groups/zamanian_group/output/
 
 #move the output into a different folder named sra
