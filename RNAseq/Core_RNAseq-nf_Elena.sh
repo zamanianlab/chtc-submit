40cfb036f44d9a67c5ac068ca68f4f52a66ace64
Elena Rehborg <106282062+ejgrehborg@users.noreply.github.com>
2025-10-15 18:30:57 -0500

    Update Core_RNAseq-nf_Elena.sh

--- RNAseq/Core_RNAseq-nf_Elena.sh
+++ RNAseq/Core_RNAseq-nf_Elena.sh
@@ -9,12 +9,15 @@ mkdir input work output
 # grep 'cpu cores' /proc/cpuinfo | uniq
 # echo $(free -g)
 
-# transfer and decompress input data from staging ($1 is ${dir} from args)
-cp -r /staging/groups/zamanian_group/input/$1.tar input
-
-# to untar the file:
-# cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd .. #for RD structure
-cd input && tar -xvf $1.tar && rm $1.tar && cd .. #for brc transfer no file structure
+# copy and extract all input tar files
+for sample in "$@"; do
+    echo "Processing sample: $sample"
+    cp -r /staging/groups/zamanian_group/input/${sample}.tar input/
+    cd input
+    tar -xvf ${sample}.tar
+    rm ${sample}.tar
+    cd ..
+done
 
 # clone nextflow git repo
 git clone https://github.com/zamanianlab/Core_RNAseq-nf.git
@@ -31,21 +34,18 @@ nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir
 #  --star --release "WBPS18" --species "dirofilaria_immitis" --prjn "PRJEB1797" --rlen "150"
   --star --release "WBPS19" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
 
-
-
-# run nextflow (QC, star) for Aedes workflow
-#export NXF_OPTS='-Xms1g -Xmx8g'
-#nextflow run Core_RNAseq-nf/Ae-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1 --rlen "150"\
-
-
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work input
 
-# tar output folder and delete it
-cd output && tar -cvf $1.tar $1 && rm -r $1 && cd ..
-
-# remove staging output tar if there from previous run
-rm -f /staging/groups/zamanian_group/output/$1.tar
+# tar and move output (use a combined name)
+output_name="combined_inputs_$(date +%Y%m%d_%H%M%S)"
+cd output
+tar -cvf ${output_name}.tar ./*
+cd ..
 
 # mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
 mv output/$1.tar /staging/groups/zamanian_group/output/
+
+# move to staging area
+rm -f /staging/groups/zamanian_group/output/${output_name}.tar
+mv output/${output_name}.tar /staging/groups/zamanian_group/output/
