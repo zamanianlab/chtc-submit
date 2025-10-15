c5ee7b2c038730bcbe6f567ea235ca18b8044dbe
Elena Rehborg <106282062+ejgrehborg@users.noreply.github.com>
2025-10-15 18:24:54 -0500

    Update Core_RNAseq-nf_Elena.sh

--- RNAseq/Core_RNAseq-nf_Elena.sh
+++ RNAseq/Core_RNAseq-nf_Elena.sh
@@ -1,5 +1,4 @@
-# copy of Core_RNAseq-nf.sh
-# !/bin/bash
+#!/bin/bash
 
 # set home () and mk dirs
 export HOME=$PWD
@@ -11,22 +10,11 @@ mkdir input work output
 # echo $(free -g)
 
 # transfer and decompress input data from staging ($1 is ${dir} from args)
-cp -r /staging/groups/zamanian_group/input/$1_1.tar input
+cp -r /staging/groups/zamanian_group/input/$1.tar input
 
-# to untar the 1 file:
+# to untar the file:
 # cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd .. #for RD structure
-cd input && tar -xvf $1_1.tar && rm $1_1.tar  #for brc transfer no file structure
-
-# perform next two lines if you have multiple input folders
-# Make a combined directory
-mkdir ${1}_all
-
-# Move (or copy) all .fastq files from all mapping folders into it
-find . -type f -name "*.fastq*" -exec mv {} ${1}_all/ \;
-
-cd .. 
-
-cd work
+cd input && tar -xvf $1.tar && rm $1.tar && cd .. #for brc transfer no file structure
 
 # clone nextflow git repo
 git clone https://github.com/zamanianlab/Core_RNAseq-nf.git
@@ -37,26 +25,27 @@ export NXF_OPTS='-Xms1g -Xmx8g'
 #if one file
 # nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1\
 # if multiple files
-nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config \
-  --dir input/${1}_all  \
-  --star --release "WBPS19" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
+nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir input/${1}_all
 #   --star --release "WBPS19" --species "schistosoma_mansoni" --prjn "PRJEA36577" --rlen "50"
 #   --star --release "WBPS18" --species "dirofilaria_immitis" --prjn "PRJNA723804" --rlen "150"
 #  --star --release "WBPS18" --species "dirofilaria_immitis" --prjn "PRJEB1797" --rlen "150"
+  --star --release "WBPS19" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
+
+
 
 # run nextflow (QC, star) for Aedes workflow
 #export NXF_OPTS='-Xms1g -Xmx8g'
 #nextflow run Core_RNAseq-nf/Ae-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1 --rlen "150"\
-cd ..
+
 
 # rm files you don't want transferred back to /home/{net-id}
 rm -r work input
 
 # tar output folder and delete it
-cd output && tar -cvf ${1}_all.tar $1_all && rm -r $1_all && cd ..
+cd output && tar -cvf $1.tar $1 && rm -r $1 && cd ..
 
 # remove staging output tar if there from previous run
-rm -f /staging/groups/zamanian_group/output/${1}_all.tar
+rm -f /staging/groups/zamanian_group/output/$1.tar
 
 # mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
-mv output/${1}_all.tar /staging/groups/zamanian_group/output/
+mv output/$1.tar /staging/groups/zamanian_group/output/
