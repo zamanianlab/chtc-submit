269559b188ab84abf5715b2721f6243a40ea52df
Natalia Betancourt <nbetancourt@wisc.edu>
2026-08-05 14:30:55 -0500

    Create Core_RNAseq-nf_CELeidoscope.sh

---
+++ RNAseq/Core_RNAseq-nf_CELeidoscope.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# set home () and mk dirs
+export HOME=$PWD
+mkdir input work output
+
+# echo core, thread, and memory
+# echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
+# grep 'cpu cores' /proc/cpuinfo | uniq
+# echo $(free -g)
+
+# transfer and decompress input data from staging ($1 is ${dir} from args)
+ cp /staging/groups/zamanian_group/input/$1.tar input
+
+# to untar the file:
+ cd input && tar -xvf $1.tar && rm $1.tar && mv */*/* $1 && cd .. #for RD structure
+# cd input && tar -xvf $1.tar && rm $1.tar && cd .. #for brc transfer no file structure
+
+# clone nextflow git repo
+git clone https://github.com/zamanianlab/Core_RNAseq-nf.git
+
+# run nextflow (QC, star) for Diro workflow
+export NXF_OPTS='-Xms1g -Xmx8g'
+
+#if one file
+# nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1\
+# if multiple files
+nextflow run Core_RNAseq-nf/WB-pe_CELeidoscope.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1 \
+# --star --release "WBPS18" --species "dirofilaria_immitis" --prjn "PRJEB1797" --rlen "150"
+# --star --release "WBPS18" --species "dirofilaria_immitis" --prjn "PRJNA723804" --rlen "150"
+# nextflow run Core_RNAseq-nf/WB-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir sra_files_output \
+# --star --release "WBPS19" --species "brugia_malayi" --prjn "PRJNA10729" --rlen "150"
+ --star --release "WBPS19" --species "caenorhabditis_elegans" --prjn "PRJNA13758" --rlen "150"
+#   --star --release "WBPS19" --species "schistosoma_mansoni" --prjn "PRJEA36577" --rlen "50"
+
+
+
+# run nextflow (QC, star) for Aedes workflow
+#export NXF_OPTS='-Xms1g -Xmx8g'
+#nextflow run Core_RNAseq-nf/Ae-pe.nf -w work -c Core_RNAseq-nf/chtc.config --dir $1 --rlen "150"\
+
+
+# rm files you don't want transferred back to /home/{net-id}
+rm -r work input
+
+#remove original files to save memory
+#cd sra_files_output
+#rm *_2.fq.gz
+#rm *_1.fq.gz
+#cd ..
+
+# tar output folder and delete it - for when there is no additional directory step between the input files and directory where NF was run
+ cd output && tar -cvf $1.tar $1 && rm -r $1 && cd ..
+# EGR - when running genome mapping from a denovo assembly, the files and directory published to from nextflow is sra_files_output
+# tar -cvf output/$1.tar sra_files_output && rm -r sra_files_output
+# remove staging output tar if there from previous run
+rm -f /staging/groups/zamanian_group/output/$1.tar
+
+# mv large output files to staging output folder; avoid their transfer back to /home/{net-id}
+mv output/$1.tar /staging/groups/zamanian_group/output/
